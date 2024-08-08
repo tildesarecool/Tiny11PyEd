ddba1f5f1aaffad7670242dfed1840ed6c7b3a21
tildesarecool <tildesare.cool@gmail.com>
2024-08-08 10:36:53 -0700

    updated globals

--- copyISOContent.py
+++ copyISOContent.py
@@ -1,16 +1,16 @@
 
 
-from globals import srcPath, WImfillPath, ESDfillPath, tempDir, sample_input, menu_items, ESDPathAlien, defaultTinyPath, defaultTinyPathWin11, appxPackagesToRemove
+from globals import srcPath, WImfillPath, ESDfillPath, tempDir, sample_input, menu_items, ESDPathAlien, defaultTinyPath, defaultTinyPathWin11, appxPackagesToRemove, FAT32_MAX_FILE_SIZE
 from typing import Union
 
 from helper_fun import is_dism_available, checkUserInputYorN, converIndexList, CheckOnMkDir, checkIfPathExists, GetWIMinfoReturnFormatted, convertESDtoWIM, ValidateSanitizePath
 
 import multiprocessing as mp
 from multiprocessing import Pool
-import os, shutil, time, threading, argparse, sys #, msgpack
+import os, shutil, time, threading, argparse, sys, msgpack
 
 #FAT32_MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4 GB
 
-FAT32_MAX_FILE_SIZE = 4 * 1024**3  # 4 GB
 
+#print(f"number is {FAT32_MAX_FILE_SIZE}")
 
--- globals.py
+++ globals.py
@@ -3,6 +3,8 @@ import os
 srcPath = None
 tempDir = None
 
+FAT32_MAX_FILE_SIZE = 4 * 1024**3  # 4 GB
+
 # packages to remove. Use '#' to comment out any you don't want removed.
 appxPackagesToRemove: tuple = (
 'Clipchamp.Clipchamp_', 
